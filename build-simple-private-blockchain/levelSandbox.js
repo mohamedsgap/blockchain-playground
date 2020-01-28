f6131f4ff582f96a9c1109b68a6c718d7d45936e
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-01-28 10:54:41 +0200

    handel data with test

--- build-simple-private-blockchain/levelSandbox.js
+++ build-simple-private-blockchain/levelSandbox.js
@@ -14,3 +14,60 @@ export default function addLevelDBData(key,value){
     });
   });
 }
+
+
+// Get data from levelDB with key
+export default function getLevelDBData(key){
+  return new Promise(function(resolve, reject) {
+    db.get(key, function(err, value) {
+      if (err) {
+        if (err.type == 'NotFoundError') {
+          resolve(undefined);
+        } else {
+          console.log('Block ' + key + ' get failed', err);
+          reject(err);
+        }
+      } else {
+        resolve(value);
+      }
+    });
+  });
+}
+
+// Add data to levelDB with value
+export default function addDataToLevelDB(value) {
+  return new Promise(function(resolve, reject) {
+    let i = 0;
+    db.createReadStream()
+      .on('data', function(data) {
+        i++;
+      })
+      .on('error', function(err) {
+        console.log('Unable to read data stream!', err);
+        reject(err);
+      })
+      .on('close', function() {
+        console.log('Block #' + i);
+        resolve(addLevelDBData(i, value));
+      });
+  });
+}
+
+/* ===== Testing ==============================================================|
+|  - Self-invoking function to add blocks to chain                             |
+|  - Learn more:                                                               |
+|   https://scottiestech.info/2014/07/01/javascript-fun-looping-with-a-delay/  |
+|                                                                              |
+|  * 100 Milliseconds loop = 36,000 blocks per hour                            |
+|     (13.89 hours for 500,000 blocks)                                         |
+|    Bitcoin blockchain adds 8640 blocks per day                               |
+|     ( new block every 10 minutes )                                           |
+|  ===========================================================================*/
+
+
+(function theLoop (i) {
+  setTimeout(function () {
+    addDataToLevelDB('Testing data');
+    if (--i) theLoop(i);
+  }, 100);
+})(10);
