122287c51cdcb419f3d169ad0dd9d822c9c9724f
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-01-28 10:48:16 +0200

    feat: validate blocks

--- build-simple-private-blockchain/simpleChain.js
+++ build-simple-private-blockchain/simpleChain.js
@@ -1,5 +1,3 @@
-
-
 const SHA256 = require('crypto-js/sha256');
 
 
@@ -53,3 +51,44 @@ class Blockchain{
       // return object as a single string
       return JSON.parse(JSON.stringify(this.chain[blockHeight]));
     }
+
+    // validate block
+    validateBlock(blockHeight){
+      // get block object
+      let block = this.getBlock(blockHeight);
+      // get block hash
+      let blockHash = block.hash;
+      // remove block hash to test block integrity
+      block.hash = '';
+      // generate block hash
+      let validBlockHash = SHA256(JSON.stringify(block)).toString();
+      // Compare
+      if (blockHash===validBlockHash) {
+          return true;
+        } else {
+          console.log('Block #'+blockHeight+' invalid hash:\n'+blockHash+'<>'+validBlockHash);
+          return false;
+        }
+    }
+
+   // Validate blockchain
+    validateChain(){
+      let errorLog = [];
+      for (var i = 0; i < this.chain.length-1; i++) {
+        // validate block
+        if (!this.validateBlock(i))errorLog.push(i);
+        // compare blocks hash link
+        let blockHash = this.chain[i].hash;
+        let previousHash = this.chain[i+1].previousBlockHash;
+        if (blockHash!==previousHash) {
+          errorLog.push(i);
+        }
+      }
+      if (errorLog.length>0) {
+        console.log('Block errors = ' + errorLog.length);
+        console.log('Blocks: '+errorLog);
+      } else {
+        console.log('No errors detected');
+      }
+    }
+}
